32ab4e4c8135d777f7b5c6e54c2b231f60d98d6d
Detektive-one <78425489+Detektive-one@users.noreply.github.com>
2023-07-13 09:25:48 +0530

    Update signing-server.py

--- signing-server.py
+++ signing-server.py
@@ -3,6 +3,44 @@ import json
 from eth_account import Account
 from eth_account.messages import encode_defunct
 
+
+class VsockListener:
+    """Server"""
+    def __init__(self, conn_backlog=128):
+        self.conn_backlog = conn_backlog
+
+    def bind(self, port):
+        """Bind and listen for connections on the specified port"""
+        self.sock = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+        self.sock.bind((socket.VMADDR_CID_ANY, port))
+        self.sock.listen(self.conn_backlog)
+
+    def recv_data(self):
+        """Receive data from a remote endpoint"""
+        received_data = b""
+        (from_client, (remote_cid, remote_port)) = self.sock.accept()
+
+        while True:
+            try:
+                data = from_client.recv(1024)
+            except socket.error:
+                break
+            if not data:
+                break
+            received_data += data
+
+        from_client.close()
+        return received_data
+
+
+    def send_data(self, data):
+        """Send data to a renote endpoint"""
+        while True:
+            (to_client, (remote_cid, remote_port)) = self.sock.accept()
+            to_client.sendall(data)
+            to_client.close()
+
+
 def sign_transaction(private_key, transaction_payload):
     # Convert the private key to an Ethereum account
     account = Account.from_key(private_key)
@@ -16,53 +54,44 @@ def sign_transaction(private_key, transaction_payload):
     # Return the signed transaction and transaction hash
     return signed_txn.rawTransaction.hex(), txn_hash.hex()
 
+def send_ec2_server(payload,server):
+    
+   
+    
 
-def main():
-    print("Signing server running...")
+    payload_json = json.loads(payload.decode())
+    private_key = payload_json["private_key"]
+    transaction_payload = payload_json["transaction_payload"]  
 
-    # Create a vsock socket object
-    s = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+    try:
+        # Sign the transaction inside the enclave
+        signed_tx, tx_hash = sign_transaction(private_key, transaction_payload)
 
-    # Listen for connection from any CID
-    cid = socket.VMADDR_CID_ANY
+        # Prepare response payload
+        response_payload = {
+            "signed_transaction": signed_tx,
+            "transaction_hash": tx_hash
+        }
 
-    # The port should match the client running in the EC2 server
-    port = 5005
-
-    # Bind the socket to CID and port
-    s.bind((cid, port))
-
-    # Listen for connection from EC2 server
-    s.listen()
-
-    while True:
-        c, addr = s.accept()
-
-        # Receive transaction payload and private key from EC2 server
-        payload = c.recv(4096)
-        payload_json = json.loads(payload.decode())
-
-        private_key = payload_json["private_key"]
-        transaction_payload = payload_json["transaction_payload"]
-
-        try:
-            # Sign the transaction inside the enclave
-            signed_tx, tx_hash = sign_transaction(private_key, transaction_payload)
-
-            # Prepare response payload
-            response_payload = {
-                "signed_transaction": signed_tx,
-                "transaction_hash": tx_hash
-            }
-
-        except Exception as e:
-            response_payload = {
-                "error": str(e)
-            }
+    except Exception as e:
+        response_payload = {
+            "error": str(e)
+        }
 
         # Send the response payload back to the EC2 server
-        c.send(json.dumps(response_payload).encode())
-        c.close()
+        
+        server.send_data(json.dumps(response_payload).encode())
+        
+
+def main():
+    print("Signing server running...")    
+    port = 5005
+
+    server = VsockListener()
+    server.bind(socket.VMADDR_CID_ANY,port)  
+    payload = server.recv_data(4096)
+
+    send_ec2_server(payload,server)
 
 
 if __name__ == '__main__':
