34443a55d9180a36b86eb57e977a8f358db1e153
Detektive-one <78425489+Detektive-one@users.noreply.github.com>
2023-07-25 10:22:48 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,10 +1,10 @@
 import socket
 import ssl
 
-def client():
-    # Connect to the vsock_proxy running on the host system at 16:5000.
+def client(cid, port):
+    # Connect to the vsock_proxy running on the host system at the specified CID and port.
     conn = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
-    conn.connect((16, 5000))
+    conn.connect((cid, port))
 
     # Read the API response from the vsock connection and print it.
     data = conn.recv(1024).decode()
@@ -15,4 +15,6 @@ def client():
     conn.close()
 
 if __name__ == "__main__":
-    client()
+    cid = int(input("Enter the CID of the vsock_proxy: "))
+    port = int(input("Enter the port number of the vsock_proxy: "))
+    client(cid, port)
